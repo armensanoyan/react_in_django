cc53bd5cae18bab561fa81301c8f64e2bad86782
Armen Sanoyan <armensanoyan@gmail.com>
2021-03-28 15:34:53 +0400

    updated settings of whitenoise

--- django_for_react/wsgi.py
+++ django_for_react/wsgi.py
@@ -2,9 +2,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_for_react.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
