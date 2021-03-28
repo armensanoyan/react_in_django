0e07054e689ae8c6c7b4dc1f7948e9e5da62aa38
Armen Sano <armensano@as.mac>
2021-03-28 14:22:28 +0400

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Here is my second step in django/react app</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
